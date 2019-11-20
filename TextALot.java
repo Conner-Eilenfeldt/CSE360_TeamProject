8da5de6311ce64399329e5ccb6d81909250c8b5a
Conner-Eilenfeldt <55855605+Conner-Eilenfeldt@users.noreply.github.com>
2019-11-20 15:07:32 -0700

    Add files via upload

---
+++ TextALot.java
@@ -0,0 +1,111 @@
+package teamProject;
+
+public class TextALot
+{
+	int MAX_LINE_LENGTH = 150;
+
+	int lineLength = 80;
+	int justification = 0; // 0 = left, 1 = center, 2 = right
+	int paragraph = 0; // number of spaces for new paragraph
+	boolean equalSpacing = false; // true = equal spacing
+	boolean wrapping = false; // true = wrapped text
+	boolean column = false; // true = 2 columns
+	//	ArrayList<String> array;
+
+	String input;
+
+
+	int main ()
+	{
+		String output;
+		int number;
+		input = getline();
+
+		if (input.charAt(0) == '-')
+		{
+			switch(input.charAt(1))
+			{
+			case 'n':
+				try
+				{
+					number = Integer.parseInt(input.substring(2));
+					lineLength = number;
+				}
+				catch(Exception e)  // change e
+				{
+					System.out.print("error");
+				}
+				break;
+			
+			case 'l':
+				justification = 0;
+				break;
+				
+			case 'c':
+				justification = 1;
+				break;
+				
+			case 'r':
+				justification = 2;
+				break;
+				
+			case 'e':
+				equalSpacing = !equalSpacing;
+				break;
+				
+			case 'w':
+				// wrapping method
+				if(input.equals("-w+"))
+					wrapping = true;
+				else if(input.equals("-w-"))
+					wrapping = false;
+				else
+					//command error
+				break;
+				
+			case 'p':
+				try
+				{
+					number = Integer.parseInt(input.substring(2));
+					paragraph = number;
+				}
+				catch(Exception e)  // change e
+				{
+					System.out.print("error");
+				}
+				
+				break;
+			case 'b':
+				try
+				{
+					number = Integer.parseInt(input.substring(2));
+					// blankLine(number);
+				}
+				catch(Exception e)  // change e
+				{
+					System.out.print("error");
+				}
+				
+				break;
+				
+			case 't':
+				// title method
+				break;
+				
+			case 'a':
+				// column method
+				break;
+			}
+		}
+		else
+		{
+			if (column)
+				output = columnFormat(input);
+			else
+				output = format(input);
+
+			System.out.print(output);
+		}
+	}
+
+}
