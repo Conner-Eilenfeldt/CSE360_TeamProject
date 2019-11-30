05fed72625f708681ab3bea1d0b7f45bea019295
PaydenBrown <56323952+PaydenBrown@users.noreply.github.com>
2019-11-30 13:29:49 -0700

    Add files via upload

--- WrapInformation.java
+++ WrapInformation.java
@@ -1,5 +1,21 @@
 package application;
 
+/**
+ * Team Members:
+ * @author Payden Brown
+ * @author Jonathan Chance
+ * @author Conner Eilenfeldt
+ * @author Ajay Tiwari
+ * 
+ * Class ID: CSE360 85141
+ * 
+ * Assignment: Team Project TextALot
+ * Description:
+ * Class to be used in implementing a queue to keep track of all the 
+ * lines that will need to be wrapped. Tracks all the settings 
+ * specified by the user that affect wrapping. 
+ */
+
 public class WrapInformation 
 {
 	public String storedText;
@@ -8,6 +24,10 @@ public class WrapInformation
 	public boolean spacing;
 	public int lineLength;
 	
+	/**
+	 * WrapInformation Constructor
+	 * sets all information to default settings
+	 */
 	public WrapInformation()
 	{
 		storedText = "";
@@ -16,6 +36,16 @@ public class WrapInformation
 		spacing = false;
 		lineLength = 80;
 	}
+	
+	/**
+	 * WrapInformation Constructor with specified settings
+	 * @param insertText updates the text to be added 
+	 * @param allignment matches the allignment settings
+	 * @param equallySpaced is equally spaced setting on
+	 * @param singleSpaced matches settings of single or double spaced
+	 * @param lineLimit updates the line limit
+	 * updates all the settings to users specifications
+	 */
 	public WrapInformation(String insertText, int allignment,boolean equallySpaced,boolean singleSpaced,int lineLimit)
 	{
 		storedText = insertText;
